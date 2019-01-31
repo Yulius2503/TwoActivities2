e36da1ef35dc673ddcf491c38c1f38cc8d6b64e9
yulius2503 <yulius.savior@gmail.com>
2019-01-31 17:44:38 +0700

    commit

---
+++ app/src/main/java/com/project/mainactivity/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.project.mainactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
